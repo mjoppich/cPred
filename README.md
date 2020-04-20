3ee1b922e1632d63e330237cfcf74acfba3dbdb2
mjoppich <github@mjoppich.net>
2020-04-20 16:57:37 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,110 @@
 # scrnaseq_celltype_prediction
-Predict the celltype of a cluster from Seurat FindMarker data
+
+Predict the cell-type of a cluster from Seurat FindMarker data
+
+This method has not been tested on large scale data, but might actually work.
+
+If you find this worked for you, please drop a line :)
+
+## Usage
+
+Download the analyseMarkers.py script and run it on your output files.
+
+Best workflow: from within R/Seurat run the following script to generate a dataframe containing all relevant data (code not yet tested ...):
+
+    getExprData = function(markerObj, markerCells, sampleSuffix)
+    {
+    expTable = GetAssayData(object = subset(x=markerObj, cells=markerCells), slot = "data")
+        
+        outvalues1 = t(apply(expTable, 1, function(x) {
+                a=x[x > 0];
+                #a=x;
+                out = {}
+        
+                out["anum"] = length(x)
+                out["num"] = length(a)
+        
+                f = fivenum(a)
+                out["min"] = f[1]
+                out["lower_hinge"] = f[2]
+                out["median"] = f[3]
+                out["upper_hinge"] = f[4]
+                out["max"] = f[5]
+                out["mean"] = mean(a)
+        
+                out
+            }))
+        outvalues1 = cbind(rownames(outvalues1), outvalues1)
+        cnames[1] = "gene"
+        colnames(outvalues1) = cnames
+        
+        return(outvalues1)
+    }
+
+    printDEDataExpression = function ( scdata, assay="SCT" )
+    {
+
+        outDF = NULL
+    
+    DefaultAssay(object=scdata) = assay  
+    clusterIDs = as.character(sort(unique(Idents(scdata))))
+                        
+    scCells = Idents(scdata)
+    scCells = names(scCells)
+    scCells = unlist(as.character(scCells))
+
+    for (clusterID in clusterIDs){
+        
+        print(clusterID)
+        
+        cellIdents = Idents(scdata)
+        cellIdents.c = names(cellIdents[cellIdents == clusterID])
+        cellIdents.c = unlist(lapply(cellIdents.c, as.character))    
+
+        outvalues = getExprData(markerObj1, marker1Cells, suffix1)
+        outvalues = cbind(clusterID, outvalues)
+
+        if (outDF == NULL)
+        {
+            outDF = outvalues
+        } else {
+            outDF = c(outDF, outvalues)
+        }
+
+    }
+
+    return(outDF)
+    
+    }
+
+Then you can simply call
+
+    python3 analyseMarkers.py --markers example/marker_genes_single_human_all.tsv --predictions 1
+    0       Fibroblasts;Connective tissue   3.7979322878001667      62      179
+    1       Smooth muscle cells;Smooth muscle       3.209013271632466       38      82
+    2       Pancreatic stellate cells;Pancreas      2.2069340605035954      20      29
+    3       Macrophages;Immune system       5.050242002842474       63      153
+    4       Smooth muscle cells;Smooth muscle       0.9633089909462268      23      82
+    5       Endothelial cells;Vasculature   7.505313125200414       84      195
+    6       Fibroblasts;Connective tissue   2.8734895530493767      55      179
+    7       T cells;Immune system   1.4604815939740858      35      107
+    8       Fibroblasts;Connective tissue   0.7440482333103114      34      179
+    9       Fibroblasts;Connective tissue   3.883677668408906       63      179
+    10      Pericytes;Vasculature   0.803543270719957       22      64
+    11      Smooth muscle cells;Smooth muscle       1.985453728430751       37      82
+    12      B cells naive;Immune system     0.9207522036965257      19      69
+    13      Plasma cells;Immune system      13.783575029154143      39      86
+    14      Dendritic cells;Immune system   3.998198909943591       49      133
+    15      Kupffer cells;Liver     1.9725414603905738      16      49
+    16      Pancreatic stellate cells;Pancreas      3.883768523344435       18      29
+    17      Endothelial cells;Vasculature   5.854839914806603       68      195
+    18      Endothelial cells;Vasculature   6.300630147493595       78      195
+    19      Schwann cells;Brain     2.201608826888472       21      48
+    20      Fibroblasts;Connective tissue   0.5834923037164054      37      179
+    21      NK cells;Immune system  3.665464137900602       40      98
+    23      Mast cells;Immune system        1.9691749668080014      21      162
+
+and you will receive 1 prediction per cluster. In a real life scenario you might want to check more than just one.
+
+The output format is as follow:
+    cluster -> cell_type -> score -> accepted_marker_genes -> marker_genes_of_celltype
---
+++ analyseMarkers.py
@@ -0,0 +1,242 @@
+from collections import defaultdict, Counter
+import math
+import sys, os
+import argparse
+import gzip
+from natsort import natsorted
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Name Seurat clusters')
+    parser.add_argument('-i', '--markers', type=argparse.FileType('r'),help='output from Seurat FindMarkers or FindAllMarkers')
+    parser.add_argument('-u', '--update-panglao', action='store_true', default=False, help='update panglao db file')
+
+    parser.add_argument('-g', '--gene', default="gene", type=str, help="column containing cluster id value")
+    parser.add_argument('-c', '--cluster', default="clusterID", type=str, help="column containing cluster id value")
+
+    parser.add_argument('-l', '--logfc', default="avg_logFC", type=str, help="column containing cluster id value")
+    parser.add_argument('-p', '--pvaladj', default="p_val_adj", type=str, help="column containing cluster id value")
+    parser.add_argument('-e', '--expr-mean', default="mean", type=str, help="column containing cluster id value")
+
+    parser.add_argument('-ec', '--expressing-cell-count', default="num", type=str, help="column containing cluster id value")
+    parser.add_argument('-tc', '--cluster-cell-count', default="anum", type=str, help="column containing cluster id value")
+
+    parser.add_argument('-n', '--predictions', default=10, type=int, help="number of predictions per cluster shown")
+
+    
+    args = parser.parse_args()
+
+
+
+    gene2clusters = defaultdict(set)
+
+    with args.markers as fin:
+
+        cluster2genes = defaultdict(lambda: dict())
+
+        elem2idx = {}
+        idx2elem = {}
+        for idx, line in enumerate(fin):
+
+            line = line.strip().split("\t")
+
+            if idx == 0:
+
+                for lidx, elem in enumerate(line):
+
+                    elem2idx[elem] = lidx
+                    idx2elem[lidx] = elem
+
+                continue
+
+            if line[0].startswith(idx2elem[0]):
+                continue
+
+            if len(line) < len(idx2elem):
+                continue
+
+            geneSym = line[elem2idx[ args.gene ]]
+            cluster = line[elem2idx[ args.cluster]]
+
+            try:
+                robustLogFC = 0
+                if args.logfc != None:
+                    robustLogFC = float(line[elem2idx[args.logfc]])
+                robustPval = 0
+                if args.logfc != None:
+                    robustPval = float(line[elem2idx[args.pvaladj]])
+                expr_value = 0
+                if args.logfc != None:
+                    expr_value = float(line[elem2idx[args.expr_mean]])
+                expr_perc = 0
+                if args.logfc != None:
+                    expr_perc = float(line[elem2idx[args.expressing_cell_count]]) / float(line[elem2idx[args.cluster_cell_count]])
+
+                if robustPval > 0.05:
+                    continue
+
+                cluster2genes[cluster][geneSym] = (expr_value, robustLogFC, robustPval, len(cluster2genes[cluster])+1, expr_perc)
+            except:
+                pass
+        
+
+    gene2refcluster = defaultdict(set)
+    nickname2gene = {}
+
+    if args.update_panglao or not os.path.isfile("panglao.tsv"):
+
+        import urllib.request
+        url = "https://panglaodb.se/markers.html?cell_type=%27all_cells%27"
+        user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:75.0) Gecko/20100101 Firefox/75.0'
+        accept_header= "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
+        request = urllib.request.Request(url,headers={'User-Agent': user_agent, 'accept': accept_header})
+        response = urllib.request.urlopen(request)
+        html = response.read()
+        
+        import lxml
+        htmldoc = lxml.html.fromstring(html)
+        allLinks = [x.attrib.get("href", None) for x in htmldoc.xpath(".//small/a")]
+
+        allLinks = [x for x in allLinks if not x == None]
+
+        for link in allLinks:
+            if link.startswith("markers/"):
+
+                if link.endswith(".gz"):
+                    with urllib.request.urlopen("https://panglaodb.se/{}".format(link)) as dl_file:
+                        with gzip.open(dl_file, 'rb') as fin:
+                            with open("panglao.tsv", 'wb') as outfile:
+                                outfile.write(fin.read())
+
+                else:
+
+                    with urllib.request.urlopen("https://panglaodb.se/{}".format(link)) as dl_file:
+                        with open("panglao.tsv", 'wb') as out_file:
+                            out_file.write(dl_file.read())
+
+            break
+
+    with open("panglao.tsv") as fin:
+
+        clusterid2genes = defaultdict(lambda: dict())
+
+        elem2idx = {}
+        for idx, line in enumerate(fin):
+
+            line = line.strip().split("\t")
+
+            if idx == 0:
+
+                for lidx, elem in enumerate(line):
+                    elem2idx[elem] = lidx
+
+                continue
+
+            geneSym = line[elem2idx["official gene symbol"]]
+            celltype = line[elem2idx["cell type"]]
+            organ = line[elem2idx["organ"]]
+
+            try:
+                meanSens = max(float(line[elem2idx["sensitivity_human"]]), float(line[elem2idx["sensitivity_mouse"]]))# / 2.0
+            except:
+                
+                if line[elem2idx["sensitivity_human"]] != "NA":
+                    meanSens = float(line[elem2idx["sensitivity_human"]])
+                elif line[elem2idx["sensitivity_mouse"]] != "NA":
+                    meanSens = float(line[elem2idx["sensitivity_mouse"]])
+                else:
+                    meanSens = 0
+
+            try:
+                meanSpec = max(float(line[elem2idx["specificity_human"]]) + float(line[elem2idx["specificity_mouse"]]))# / 2.0
+            except:
+                
+                if line[elem2idx["specificity_human"]] != "NA":
+                    meanSpec = float(line[elem2idx["specificity_human"]])
+                elif line[elem2idx["specificity_mouse"]] != "NA":
+                    meanSpec = float(line[elem2idx["specificity_mouse"]])
+                else:
+                    meanSpec = 0
+
+
+            gene2refcluster[geneSym].add((celltype, organ))
+
+            clusterid2genes[(celltype, organ)][geneSym] = ( meanSens, meanSpec)
+
+            nicknames = line[elem2idx["nicknames"]]
+            if len(nicknames) > 0:
+
+                nicknames = nicknames.split("|")
+
+                for nickn in nicknames:
+                    nickname2gene[nickn] = geneSym
+
+
+
+    for cluster in natsorted([x for x in cluster2genes]):
+
+        clusterGenes = [x for x in cluster2genes[cluster]]
+
+        clusterCounter = Counter()
+        cluster2accGenes = dict()
+
+        for refcluster in clusterid2genes:
+
+            refclusterGenes = [x for x in clusterid2genes[refcluster]]
+
+            totalScore = 0
+            accGenes = 0
+
+            seenGenes = set()
+
+            for gene in clusterGenes:
+
+                geneFound = False
+                if gene in refclusterGenes:
+                    geneFound = True
+
+                else:
+
+                    if gene in nickname2gene:
+                        gene = nickname2gene[gene]
+
+                if gene in seenGenes:
+                    continue
+
+                seenGenes.add(gene)
+                    
+                if geneFound:
+                    accGenes += 1
+                    geneInfo = cluster2genes[cluster][gene]
+                    clusterInfo = clusterid2genes[refcluster][gene]
+                    #sens, spec
+
+                    importanceInRefCluster = 1.0 / (1+math.log2(len(gene2refcluster[gene])))
+
+                    meanSens = clusterInfo[0]
+                    meanSpec = clusterInfo[1]
+
+                    pvalImp = 1000
+
+                    if geneInfo[2] == 0.0:
+                        pvalImp = 1000
+                    else:
+                        pvalImp = -math.log(geneInfo[2], 2)
+
+
+                    avgExpr = geneInfo[0]
+                    logFC = geneInfo[1]
+                    geneRank = geneInfo[3]
+                    prevInCluster = geneInfo[4]
+
+                    fGeneRank = 1 / math.log(geneRank+1, 2)
+
+                    if logFC > 0:
+                        #was  meanSens * logFC * prevInCluster
+                        totalScore += meanSens * logFC  * (1.0-meanSpec) * importanceInRefCluster #(1-meanSpec) *fGeneRank *pvalImp
+                        
+            clusterCounter[refcluster] = totalScore * (accGenes/len(clusterid2genes[refcluster])) # len(clusterid2genes[refcluster])
+            cluster2accGenes[refcluster] = accGenes
+
+        for x in clusterCounter.most_common(args.predictions):
+            print(cluster, ";".join(x[0]), x[1], cluster2accGenes[x[0]], len(clusterid2genes[x[0]]), sep="\t")
